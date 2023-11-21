a7571423e5d7482c3880b020e9f135e1efc6b888
yanguerrero <yanina.bostera15@gmail.com>
2023-11-20 22:40:12 -0300

    entrega sprint

---
+++ JS/alertas.js
@@ -0,0 +1,7 @@
+let edad  = prompt ("¿Que año es?")
+let año = 2023
+if (edad >=año) {
+    alert("Correcto! Se gano un helado");}
+else {
+    alert("El año es 2023,  pedimos la devolución del helado")
+}
